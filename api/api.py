4703550d4699548e2d813a06d1237156515e7dc2
Hamse Hashi <hamseah@stud.ntnu.no>
2019-02-03 15:20:46 +0100

    added spotify api stuff

--- api/api.py
+++ api/api.py
@@ -1,14 +1,23 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
+import spotify_api
 
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'Hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+@app.route('/user/<username>', methods=['GET'])
+def get_user(username):
+        user = {}
+        spotify_user = spotify_api.get_user(username)
+        if spotify_user:
+            user['id'] = spotify_user['id']
+            user['display_name'] = spotify_user['display_name']
+            user['followers'] = spotify_user['followers']['total']
+            return jsonify({ 'user': user}), 200
+        else:
+            return jsonify({'error': 'did not find the user'}), 404
+
+
+#api.add_resource(Spotify_API, '/spotify/user/<username>', endpoint='get_user')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80,debug=True)
+    app.run(debug=True)
---
+++ api/spotify_api.py
@@ -0,0 +1,33 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+print(sp)
+
+# API call to get the current user info
+# GET: 200 
+def get_user(username):
+    user = sp.user(username)
+    return user
+
+# GET: retrieve an artist by ID
+def get_artist():
+    # Eminem ID= 7dGJo4pcD2V6oG8kP0tJRR
+    artist = sp.artist('7dGJo4pcD2V6oG8kP0tJRR')
+    print(artist)
+
+# GET: gets an artist's top tracks, use COUNTY to limit the request
+def get_artist_top_tracks():
+    top_tracks = sp.artist_top_tracks('2YZyLoL8N0Wb9xBt1NhZWg')
+    for tracks in top_tracks['tracks']:
+        for album in tracks:
+            if album == 'album':
+                #print('name: ', tracks['album']['name'], '\n')
+                #print('total tracks: ', tracks['album']['total_tracks'], '\n')
+                #print('release date: ', tracks['album']['release_date'], '\n')
+                print('key: ', tracks['album'], '\n')   
+
+# Get 
+# get_user()
